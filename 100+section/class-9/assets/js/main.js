a34f9ace1565cfcea87755a2fd1dbd4e634c0e6f
didar3ar <didar3ar@gmail.com>
2017-11-30 11:39:22 +0600

    class-9

---
+++ 100+section/class-9/assets/js/main.js
@@ -0,0 +1,27 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".client-testimonial-list").owlCarousel({
+        	items: 3,
+        	margin: 30,
+        	nav: false,
+        	dots: true,
+        	loop: true,
+        	autoplay: true,
+        });
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
