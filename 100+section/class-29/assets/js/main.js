051e56e27a3c92173e46bc84f0e6b92c028e9c14
didar3ar <didar3ar@gmail.com>
2018-01-21 20:31:19 +0600

    class-29

---
+++ 100+section/class-29/assets/js/main.js
@@ -0,0 +1,29 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+    	$(".project-titles li").on('click', function() {
+
+
+    		$(".project-titles li").removeClass("active");
+    		$(this).addClass("active");
+
+    		var selector = $(this).attr('data-filter');
+    		$(".projects-list").isotope({
+    			filter: selector
+    		});
+    	});
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+     	jQuery(".projects-list").isotope();
+    });
+
+
+}(jQuery));	
