12f6513cd343ae7c43ac97db59e27c4e78ce226c
didar3ar <didar3ar@gmail.com>
2018-03-22 23:12:27 +0600

    class-47

---
+++ 100+section/class-47/assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".testimonial-carousel").owlCarousel({
+        	items: 1,
+        	loop: true,
+        	dots: true,
+        	nav: false,
+        	autoplay: true,
+        });
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
