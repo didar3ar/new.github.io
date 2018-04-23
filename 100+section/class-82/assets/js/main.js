2d936e2e1c5f6f4d3227a17ea59da3e7d25fdb6f
didar3ar <didar3ar@gmail.com>
2018-04-23 12:58:39 +0600

    class-82

---
+++ 100+section/class-82/assets/js/main.js
@@ -0,0 +1,29 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".featured-project").owlCarousel({
+        	items: 1,
+        	nav: false,
+        	dots: true,
+        	loop: true,
+        	autoplay: true,
+        });
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
