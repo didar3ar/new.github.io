f5b48384c0025ba99586c8817e4cf7aa3740ca98
didar3ar <didar3ar@gmail.com>
2018-04-26 13:15:47 +0600

    class-85

---
+++ 100+section/class-85/assets/js/main.js
@@ -0,0 +1,17 @@
+    jQuery(document).ready(function($){
+
+
+        $(".testimonial-carousel").owlCarousel({
+        	items: 1,
+        	dots: false,
+        	nav: true,
+        	navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        	loop: true,
+        	autoplay: false
+        });
+
+
+        
+
+
+    });
