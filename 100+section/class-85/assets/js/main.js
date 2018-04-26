c3d84b9c7d7581d92e576c373a34b8da361a954d
didar3ar <didar3ar@gmail.com>
2018-04-26 13:17:35 +0600

    Update main.js

--- 100+section/class-85/assets/js/main.js
+++ 100+section/class-85/assets/js/main.js
@@ -7,7 +7,7 @@
         	nav: true,
         	navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
         	loop: true,
-        	autoplay: false
+        	autoplay: true
         });
 
 
